f553bed1fb726d475152f3b9ed80337645bb5dfe
Danielle Rubin <dlhrubin@gmail.com>
2020-01-06 17:59:26 -0500

    Re-style project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,7 @@ import projects from "../data.js";
 export class Projects extends Component {
     render() {
         let projectCards = projects.map((proj, i) => {
+            let stack = proj.stack.map(word => <span className="tech" key={word}>{word}</span>)
             return (
                 <div key={i} className="card">
                     <div className="image-container">
@@ -13,9 +14,17 @@ export class Projects extends Component {
                         <h2>{proj.name}</h2>
                         <div className="details">
                             <p>{proj.summary}</p>
-                            <span className="stack">STACK: </span><span className="tech">{proj.stack}</span>
-                            <br></br>
-                            <span className="links"><a href={proj.live} target="_blank">LIVE</a> | <a href={proj.code} target="_blank">CODE</a></span>
+                            <div>
+                                {stack}
+                                <br></br>
+                                <div className="links">
+                                    <a className="live-link" href={proj.live} target="_blank">Live
+                                        <i className="fas fa-desktop"></i>                                    </a>
+                                    <a className="code-link" href={proj.code} target="_blank">Code
+                                        <i className="fas fa-code"></i>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/data.js
+++ src/data.js
@@ -8,15 +8,15 @@ const projects = [
         name: "Album Collage", 
         screencap: img1, 
         summary: "Album Collage is a tool for generating collages of album covers. Users can select any artist and album available in the Last.fm database and drag-and-drop in the selection box to re-order album selections. Collage shapes are dynamically generated for each possible number of album covers. Collages can be edited, shuffled, reset, and deleted.", 
-        stack: "React, JavaScript, HTML, SCSS, Axios",
+        stack: ["React", "JavaScript", "HTML", "SCSS", "Axios"],
         live: "https://www.albumcollage.com",
         code: "https://github.com/dlhrubin/album-collage"                  
     },
     {
         name: "Momentum Clone", 
         screencap: img2, 
-        summary: "This project replicates the front-end functionality of Momentum, a popular home screen extension for Google Chrome. It includes a clock and greeting based on time of day, the current weather given location, a customizable list of links, a Google search bar, a to do list, an inspirational quote, and a background image of nature. The user can set a \"main focus\" for the day and customize their name.",
-        stack: "JavaScript, HTML, CSS",
+        summary: "This vanilla JavaScript project replicates the front-end functionality of the Google Chrome extension Momentum. It includes a clock and timely greeting, the current weather given location, a customizable list of links, a Google search bar, a to do list, an inspirational quote, and a background image. The user can set a \"main focus\" for the day and customize their name.",
+        stack: ["JavaScript", "HTML", "CSS"],
         live: "https://chingu-voyages.github.io/v11-toucans-team-06/",
         code: "https://github.com/chingu-voyages/v11-toucans-team-06"
     }, 
@@ -24,7 +24,7 @@ const projects = [
         name: "Web Template Clone", 
         screencap: img3, 
         summary: "This responsive template was created as a clone of the Conquer template on templatemo. It includes a basic template with headers and content cards, a parallax effect on header images, responsive design, and enhanced user experience with smooth transitions and scrolling using vanilla JavaScript.",
-        stack: "JavaScript, HTML, CSS",
+        stack: ["JavaScript", "HTML", "CSS"],
         live: "https://dlhrubin.github.io/web-template-clone/", 
         code: "https://github.com/dlhrubin/web-template-clone"
     },
@@ -32,7 +32,7 @@ const projects = [
         name: "Portfolio", 
         screencap: img4,
         summary: "My personal website and portfolio was built with React. It includes my basic information, contact links, and project links and descriptions on dynamically populated content cards.",
-        stack: "React, JavaScript, HTML, SCSS",
+        stack: ["React", "JavaScript", "HTML", "SCSS"],
         live: "https://github.com/dlhrubin/portfolio",
         code: "https://github.com/dlhrubin/portfolio"
     }
