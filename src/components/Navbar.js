f88073e089994a00d78364b170c9d25e793428e4
Danielle Rubin <dlhrubin@gmail.com>
2020-01-07 18:53:39 -0500

    Implement smooth scrolling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,27 @@
 import React, {Component} from "react";
+import {Link} from "react-scroll";
 
 export class Navbar extends Component {
     render() {
         return(
             <nav className="navbar">
-                <a href="#landing">
-                    <i className="fas fa-user-circle"></i>
-                </a>
-                <a href="#projects">
-                    <i className="fas fa-code"></i>
-                </a>
-                <a href="#contact">
-                    <i className="far fa-envelope"></i>
-                </a>
+                <ul>
+                    <li>
+                        <Link activeClass="active" to="landing" spy={true} smooth={true}>
+                            <i className="fas fa-user-circle"></i>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link activeClass="active" to="projects" spy={true} smooth={true}>
+                            <i className="fas fa-code"></i>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link activeClass="active" to="contact" spy={true} smooth={true}>
+                            <i className="far fa-envelope"></i>
+                        </Link>
+                    </li>
+                </ul>
             </nav>
         )
     }
