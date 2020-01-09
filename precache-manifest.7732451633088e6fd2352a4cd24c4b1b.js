0ee3324cd63463c60bd704431fd65697090a3c9b
Danielle Rubin <dlhrubin@gmail.com>
2020-01-09 13:23:05 -0500

    Updates

---
+++ precache-manifest.7732451633088e6fd2352a4cd24c4b1b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9077925d5ff0d053d8e664fdfc1d6920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30fef2d61234c9f2807f",
+    "url": "/static/css/main.52210645.chunk.css"
+  },
+  {
+    "revision": "31457ea99211b91b776e",
+    "url": "/static/js/2.0624ab1b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0624ab1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30fef2d61234c9f2807f",
+    "url": "/static/js/main.6c414393.chunk.js"
+  },
+  {
+    "revision": "00ccf1beb4d435cfbe60",
+    "url": "/static/js/runtime-main.c7cc6e23.js"
+  },
+  {
+    "revision": "e8e79906fb8c5bd56a184bd1744d69e1",
+    "url": "/static/media/album_collage_screencap.e8e79906.PNG"
+  },
+  {
+    "revision": "5e6ee035ee3ca77f20d45cdc83508731",
+    "url": "/static/media/marble.5e6ee035.jpg"
+  },
+  {
+    "revision": "f5ee7efa8f9cbee4b8de3f5943740836",
+    "url": "/static/media/momentum_clone_screencap.f5ee7efa.PNG"
+  },
+  {
+    "revision": "9cb6803ac6cad584b98492ce4e9832e3",
+    "url": "/static/media/portfolio_screencap.9cb6803a.PNG"
+  },
+  {
+    "revision": "2ff0d861bbf8adbced5d2a4081e353a7",
+    "url": "/static/media/web_template_screencap.2ff0d861.PNG"
+  }
+]);
