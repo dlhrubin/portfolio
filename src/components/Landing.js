89bbd5a0d42846e7c377f2d2328cac08dd96ad9c
Danielle Rubin <dlhrubin@gmail.com>
2020-02-12 12:47:12 -0500

    Fix overlays for larger screen sizes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,7 @@ export class Landing extends Component {
             <section id="landing">
                 <div className="intro">
                     <h1>Hi, I'm Danielle Rubin</h1>
-                    <p>Thanks for stopping by! I'm a bioinformatician-turned-developer building things with <span>React</span>, vanilla <span>JavaScript</span>, <span>Sass</span>, and <span>HTML</span>. I've also worked with <span>Python</span>, <span>bash</span>, and <span>R</span>. </p>
+                    <p>I'm a bioinformatician-turned-developer building things with <span>React</span>, vanilla <span>JavaScript</span>, <span>Sass</span>, and <span>HTML</span>. I've also worked with <span>Python</span>, <span>bash</span>, and <span>R</span>. Thanks for stopping by! </p>
                 </div>
                 <i className="fas fa-angle-double-down"></i>
             </section>
